cebd6bd20a9e92e36a4a415c54c39db3f2c77efc
Adam Grare <agrare@redhat.com>
2021-03-25 16:04:47 -0400

    Add simple vcr refresher spec

---
+++ spec/models/manageiq/providers/ibm_power_vc/cloud_manager/refresher_spec.rb
@@ -0,0 +1,31 @@
+describe ManageIQ::Providers::IbmPowerVc::CloudManager::Refresher do
+  let(:ems) { FactoryBot.create(:ems_ibm_power_vc_with_vcr_authentication) }
+
+  describe "#refresh" do
+    context "full refresh" do
+      it "Performs a full refresh" do
+        2.times do
+          with_vcr { refresh(ems) }
+
+          assert_ems
+        end
+      end
+    end
+
+    def assert_ems
+      expect(ems.last_refresh_error).to be_nil
+      expect(ems.last_refresh_date).not_to be_nil
+    end
+
+    def with_vcr(suffix = nil, &block)
+      cassette_name = described_class.name.underscore
+      cassette_name += "_#{suffix}" if suffix
+
+      VCR.use_cassette(cassette_name, &block)
+    end
+
+    def refresh(targets)
+      described_class.refresh(Array(targets))
+    end
+  end
+end
