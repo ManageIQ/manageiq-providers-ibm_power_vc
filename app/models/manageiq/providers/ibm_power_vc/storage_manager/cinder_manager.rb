641d4d4202297e023feeb34ca6435dba35e75d71
Adam Grare <adam@grare.com>
2021-04-26 14:34:38 -0400

    Fix network/storage subclass ems_types

--- app/models/manageiq/providers/ibm_power_vc/storage_manager/cinder_manager.rb
+++ app/models/manageiq/providers/ibm_power_vc/storage_manager/cinder_manager.rb
@@ -2,4 +2,12 @@ ManageIQ::Providers::Openstack::StorageManager::CinderManager.include(ActsAsStiL
 
 class ManageIQ::Providers::IbmPowerVc::StorageManager::CinderManager < ManageIQ::Providers::Openstack::StorageManager::CinderManager
   require_nested :EventCatcher
+
+  def self.ems_type
+    @ems_type ||= "ibm_power_vc_cinder".freeze
+  end
+
+  def self.description
+    @description ||= "IBM PowerVC Cinder".freeze
+  end
 end
