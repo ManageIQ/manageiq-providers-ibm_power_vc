f4e482d24d414ab5451ccbcff0e4b0daeafdd861
Adam Grare <adam@grare.com>
2021-08-31 08:11:56 -0400

    Don't subclass workers from OpenStack

--- app/models/manageiq/providers/ibm_power_vc/network_manager/event_catcher.rb
+++ app/models/manageiq/providers/ibm_power_vc/network_manager/event_catcher.rb
@@ -1,8 +1,8 @@
-class ManageIQ::Providers::IbmPowerVc::CloudManager::EventCatcher < ManageIQ::Providers::BaseManager::EventCatcher
+class ManageIQ::Providers::IbmPowerVc::NetworkManager::EventCatcher < ManageIQ::Providers::BaseManager::EventCatcher
   require_nested :Runner
 
   def self.settings_name
-    :event_catcher_ibm_power_vc
+    :event_catcher_ibm_power_vc_network
   end
 
   def self.all_valid_ems_in_zone
