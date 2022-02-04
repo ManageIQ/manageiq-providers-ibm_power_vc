2e04993d8193bbc4a2e9a0de162066982dd93720
Isgandar Valizada <isgandar.valizada@de.ibm.com>
2022-02-04 19:07:35 +0100

    adding PowerVC minimal release verification
    - minimal version supported can be set in global settings:
      ems->ems_ibm_power_vc->min_supported_rel
    
    - if min. supported release is omitted or set to 0
      then the verification check will be disabled

--- app/models/manageiq/providers/ibm_power_vc/inventory/collector/cloud_manager.rb
+++ app/models/manageiq/providers/ibm_power_vc/inventory/collector/cloud_manager.rb
@@ -1,2 +1,5 @@
 class ManageIQ::Providers::IbmPowerVc::Inventory::Collector::CloudManager < ManageIQ::Providers::Openstack::Inventory::Collector::CloudManager
+  def powervc_release
+    @manager.powervc_release
+  end
 end
