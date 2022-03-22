9d1b4edfc674c3deb01105acdc404d1fcbbe0404
Kuldip-Nanda <Kuldip.Nanda@ibm.com>
2022-03-22 17:53:00 -0400

    adding Cloud Volume subclasses in the IbmPowerVc CinderManager

---
+++ app/models/manageiq/providers/ibm_power_vc/storage_manager/cinder_manager/cloud_volume_snapshot.rb
@@ -0,0 +1,4 @@
+ManageIQ::Providers::Openstack::StorageManager::CinderManager::CloudVolumeSnapshot.include(ActsAsStiLeafClass)
+
+class ManageIQ::Providers::IbmPowerVc::StorageManager::CinderManager::CloudVolumeSnapshot < ManageIQ::Providers::Openstack::StorageManager::CinderManager::CloudVolumeSnapshot
+end
