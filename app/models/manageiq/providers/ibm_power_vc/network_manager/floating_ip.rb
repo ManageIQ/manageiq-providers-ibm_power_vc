daa6e536251b10e75dce2aa5260041527695ef2f
Kuldip-Nanda <Kuldip.Nanda@ibm.com>
2022-03-28 09:44:40 -0400

    adding Cloud Network subclasses in IbmPowerVC NetworkManager

---
+++ app/models/manageiq/providers/ibm_power_vc/network_manager/floating_ip.rb
@@ -0,0 +1,4 @@
+ManageIQ::Providers::Openstack::NetworkManager::FloatingIP.include(ActsAsStiLeafClass)
+
+class ManageIQ::Providers::IbmPowerVc::NetworkManager::FloatingIP < ManageIQ::Providers::Openstack::NetworkManager::FloatingIP
+end
---
+++ app/models/manageiq/providers/ibm_power_vc/network_manager/network_port.rb
@@ -0,0 +1,4 @@
+ManageIQ::Providers::Openstack::NetworkManager::NetworkPort.include(ActsAsStiLeafClass)
+
+class ManageIQ::Providers::IbmPowerVc::NetworkManager::NetworkPort < ManageIQ::Providers::Openstack::NetworkManager::NetworkPort
+end
