0c11f8e3da3c3488412dadf8d0c7318be92c10ac
Jay Carman <jwcarman@us.ibm.com>
2022-03-31 13:35:52 -0500

    Remove inventory collector subclasses
    PowerVC does not require any collector overrides and thus does not need
    to maintain any subclasses.

--- app/models/manageiq/providers/ibm_power_vc/inventory.rb
+++ app/models/manageiq/providers/ibm_power_vc/inventory.rb
@@ -1,5 +1,4 @@
 class ManageIQ::Providers::IbmPowerVc::Inventory < ManageIQ::Providers::Openstack::Inventory
-  require_nested :Collector
   require_nested :Parser
   require_nested :Persister
 end
--- app/models/manageiq/providers/ibm_power_vc/inventory/collector/cloud_manager.rb
+++
@@ -1,2 +0,0 @@
-class ManageIQ::Providers::IbmPowerVc::Inventory::Collector::CloudManager < ManageIQ::Providers::Openstack::Inventory::Collector::CloudManager
-end
