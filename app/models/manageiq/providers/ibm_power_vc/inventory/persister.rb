1c6241bfc7a758f48fd0bf865d7c4bec1010ed4a
Jay Carman <jwcarman@us.ibm.com>
2022-03-31 13:38:29 -0500

    Add inventory persister storage subclasses
    Note: PowerVC only supports cinder storage.

--- app/models/manageiq/providers/ibm_power_vc/inventory/persister.rb
+++ app/models/manageiq/providers/ibm_power_vc/inventory/persister.rb
@@ -1,3 +1,8 @@
 class ManageIQ::Providers::IbmPowerVc::Inventory::Persister < ManageIQ::Providers::Openstack::Inventory::Persister
   require_nested :CloudManager
+  require_nested :StorageManager
+
+  def cinder_manager
+    manager.kind_of?(ManageIQ::Providers::IbmPowerVc::StorageManager::CinderManager) ? manager : manager.cinder_manager
+  end
 end
---
+++ app/models/manageiq/providers/ibm_power_vc/inventory/persister/storage_manager/cinder_manager.rb
@@ -0,0 +1,2 @@
+class ManageIQ::Providers::IbmPowerVc::Inventory::Persister::StorageManager::CinderManager < ManageIQ::Providers::Openstack::Inventory::Persister::StorageManager::CinderManager
+end
