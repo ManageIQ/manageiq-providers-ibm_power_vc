e8e80df1818ebddf63faef0ed14f0e3687e62b65
Jay Carman <jwcarman@us.ibm.com>
2022-04-21 15:22:35 -0500

    WIP

--- app/models/manageiq/providers/ibm_power_vc/inventory.rb
+++ app/models/manageiq/providers/ibm_power_vc/inventory.rb
@@ -1,4 +1,15 @@
 class ManageIQ::Providers::IbmPowerVc::Inventory < ManageIQ::Providers::Openstack::Inventory
   require_nested :Parser
   require_nested :Persister
+
+  def self.parser_classes_for(_ems, target)
+    case target
+    when InventoryRefresh::TargetCollection
+      [ManageIQ::Providers::IbmPowerVc::Inventory::Parser::CloudManager,
+       ManageIQ::Providers::IbmPowerVc::Inventory::Parser::NetworkManager,
+       ManageIQ::Providers::IbmPowerVc::Inventory::Parser::StorageManager::CinderManager]
+    else
+      super
+    end
+  end
 end
