bc76fa543aa42119446d039c5c1bb43b9b4a958b
Aliza Peikes <aliza.peikes@ibm.com>
2022-11-30 14:16:00 -0500

    Update specs according to current resource inventory

--- spec/models/manageiq/providers/ibm_power_vc/cloud_manager/refresher_spec.rb
+++ spec/models/manageiq/providers/ibm_power_vc/cloud_manager/refresher_spec.rb
@@ -1,23 +1,12 @@
 describe ManageIQ::Providers::IbmPowerVc::CloudManager::Refresher do
   let(:zone) { EvmSpecHelper.create_guid_miq_server_zone.last }
   let(:ems) { FactoryBot.create(:ems_ibm_power_vc_with_vcr_authentication, :zone => zone) }
-
-  specs = YAML.load_file(File.join(__dir__, 'state.yml'))
-  specs['resources'].each do |key, _value|
-    case key['type']
-    when 'openstack_compute_flavor_v2'
-      let(:flavorid) { key['instances'][0]['attributes']['id'] }
-    when 'openstack_identity_project_v3'
-      let(:tenantid) { key['instances'][0]['attributes']['id'] }
-    when 'openstack_compute_instance_v2'
-      let(:instid) { key['instances'][0]['attributes']['id'] }
-      let(:azone) { key['instances'][0]['attributes']['availability_zone'] }
-    when 'openstack_images_image_v2'
-      let(:imageid) { key['instances'][0]['attributes']['id'] }
-    when 'openstack_networking_network_v2'
-      let(:networkid) { key['instances'][0]['attributes']['id'] }
-    end
-  end
+  let(:tenantid) { 'c619880eed614ec382ca0326ad62753c' }
+  let(:flavorid) { 'ef3e3fab78675f63f134dd0a9c62d56c' }
+  let(:instid) { 'b8e4ca04-abd3-4c1a-b03c-c5b80fb33c49' }
+  let(:azone) { 'p8_pvm' }
+  let(:imageid) { 'a426dcdc-0d22-40dd-945a-f46b4a77bdb0' }
+  let(:networkid) { '48b48f3a-80da-4a37-a10e-4b3382f69100' }
 
   describe "#refresh" do
     context "full refresh" do
@@ -40,7 +29,7 @@ describe ManageIQ::Providers::IbmPowerVc::CloudManager::Refresher do
       expect(ems.last_refresh_error).to be_nil
       expect(ems.last_refresh_date).not_to be_nil
 
-      expect(ems.cloud_tenants.count).to be > 1
+      expect(ems.cloud_tenants.count).to be > 0
       expect(ems.availability_zones.count).to be > 2
       expect(ems.flavors.count).to be > 2
       expect(ems.vms.count).to be > 1
@@ -61,8 +50,8 @@ describe ManageIQ::Providers::IbmPowerVc::CloudManager::Refresher do
     def assert_specific_cloud_tenant
       cloud_tenant = ems.cloud_tenants.find_by(:ems_ref => tenantid)
       expect(cloud_tenant).to have_attributes(
-        :name        => "ibm-default",
-        :description => "IBM Default Tenant",
+        :name        => "cloud-global",
+        :description => "",
         :enabled     => true,
         :ems_ref     => tenantid,
         :type        => "ManageIQ::Providers::IbmPowerVc::CloudManager::CloudTenant"
@@ -72,17 +61,17 @@ describe ManageIQ::Providers::IbmPowerVc::CloudManager::Refresher do
     def assert_specific_flavor
       flavor = ems.flavors.find_by(:ems_ref => flavorid)
       expect(flavor).to have_attributes(
-        :name                 => "small",
-        :cpu_total_cores      => 2,
+        :name                 => "ef3e3fab78675f63f134dd0a9c62d56c",
+        :cpu_total_cores      => 1,
         :cpu_cores_per_socket => nil,
-        :memory               => 8_589_934_592,
+        :memory               => 4_294_967_296,
         :ems_ref              => flavorid,
         :type                 => "ManageIQ::Providers::IbmPowerVc::CloudManager::Flavor",
         :ephemeral_disk_size  => 0,
         :ephemeral_disk_count => 0,
         :root_disk_size       => 0,
         :swap_disk_size       => 0,
-        :publicly_available   => true,
+        :publicly_available   => false,
         :cpu_sockets          => 1
       )
     end
