7c45c46d4b4b84d5d304d81b44b9af23b4253b71
Jay Carman <jwcarman@us.ibm.com>
2023-03-23 10:07:53 -0500

    Add a Provision subclass

---
+++ app/models/manageiq/providers/ibm_power_vc/cloud_manager/provision.rb
@@ -0,0 +1,4 @@
+ManageIQ::Providers::Openstack::CloudManager::Provision.include(ActsAsStiLeafClass)
+
+class ManageIQ::Providers::IbmPowerVc::CloudManager::Provision < ManageIQ::Providers::Openstack::CloudManager::Provision
+end
