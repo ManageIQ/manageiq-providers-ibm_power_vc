0d5f7f35715170c43fa0c5a4f7b7b313daed94de
Nasar Khan <nasarak98@gmail.com>
2024-10-31 14:04:15 -0400

    delegate child manager refresh_ems to parent cloud manager

--- app/models/manageiq/providers/ibm_power_vc/network_manager.rb
+++ app/models/manageiq/providers/ibm_power_vc/network_manager.rb
@@ -1,6 +1,10 @@
 ManageIQ::Providers::Openstack::NetworkManager.include(ActsAsStiLeafClass)
 
 class ManageIQ::Providers::IbmPowerVc::NetworkManager < ManageIQ::Providers::Openstack::NetworkManager
+  class << self
+    delegate :refresh_ems, :to => ManageIQ::Providers::IbmPowerVc::CloudManager
+  end
+
   def self.ems_type
     @ems_type ||= "ibm_power_vc_network".freeze
   end
